5fcc4ae11dbd85137bb49c407ebdc9bf90393a4c
TheTTimeLives <37144865+TheTTimeLives@users.noreply.github.com>
2018-03-07 12:10:08 -0500

    First commit
    Added to Github

---
+++ Fort/Class1.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class thiswhateverscript : Monobehaviour
+{
+    public int health = 5;
+    public float healthFloat = 5.0f;
+    public bool trueorfalse = true;
+    public GameObject player;
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+
+    void Death()
+    {
+
+    }
+	
+    void OnTriggerEnter (Collider other)
+    {
+
+    }
+
+    public void NewDeath()
+    {
+
+    }
+}
---
+++ Fort/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Thisisjjustsomedingus
+
+public class Class2
+{
+	class Program
+	{
+        static void Main(stringh[] args)
+        {
+            Console.WriteLine("Dingus");
+        }
+	}
+}
